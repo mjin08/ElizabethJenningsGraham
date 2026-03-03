430e79b6815d4ca945033ca786f992ce59b8cc5c
mjin08 <maggie.jin22@gmail.com>
2026-03-03 12:04:49 -0500

    clean up code (remove unused avatar screen)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MenuScreen from './screens/MenuScreen';
-import AvatarScreen from './screens/AvatarScreen';
 import PlayerWindow from './screens/PlayerWindow';
 import './App.css';
 
@@ -10,7 +9,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MenuScreen />} />
-        <Route path="/avatar" element={<AvatarScreen />} />
         <Route path="/player" element={<PlayerWindow />} />
       </Routes>
     </BrowserRouter>
